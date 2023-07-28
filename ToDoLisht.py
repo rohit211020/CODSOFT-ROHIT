e9af30dc44ecb300949275994c9596e70330c01d
rohit211020 <140632947+rohit211020@users.noreply.github.com>
2023-07-28 21:38:39 +0530

    Add files via upload

--- ToDoLisht.py
+++ ToDoLisht.py
@@ -38,20 +38,15 @@ def insertTask():
  
 	tasks_list.append(content)
 
-	
 	TextArea.insert('end -1 chars', "[ " + str(counter) + " ] " + content)
 
-	
 	counter += 1
 
-	
 	clear_taskField()
-
 
 def delete() :
 	
 	global counter
-	
 	
 	if len(tasks_list) == 0 :
      
@@ -59,9 +54,7 @@ def delete() :
   
 		return
 
-	
 	number = taskNumberField.get(1.0, END)
-
 	
 	if number == "\n" :
      
@@ -73,19 +66,14 @@ def delete() :
      
 		task_no = int(number)
 
-	
 	clear_taskNumberField()
-	
 	
 	tasks_list.pop(task_no - 1)
 
-	
 	counter -= 1
-	
 	
 	TextArea.delete(1.0, END)
 
-	
 	for i in range(len(tasks_list)) :
      
 		TextArea.insert('end -1 chars', "[ " + str(i + 1) + " ] " + tasks_list[i])
@@ -96,45 +84,33 @@ if __name__ == "__main__" :
 
 	gui.configure(background = "light green")
 
-	
 	gui.title("ToDo CODSOFT")
 
-	
 	gui.geometry("250x300")
 
-	
 	enterTask = Label(gui, text = "Enter Your Task", bg = "light green")
 
-	
 	enterTaskField = Entry(gui)
 
-	
 	Submit = Button(gui, text = "Submit", fg = "Black", bg = "white", command = insertTask)
 
-	
 	TextArea = Text(gui, height = 5, width = 25, font = "lucida 13")
 
-	
 	taskNumber = Label(gui, text = "Delete Task Number", bg = "white")
 						
 	taskNumberField = Text(gui, height = 1, width = 2, font = "lucida 13")
 
-	
 	delete = Button(gui, text = "Delete", fg = "Black", bg = "white", command = delete)
 
-	
 	Exit = Button(gui, text = "Exit", fg = "Black", bg = "white", command = exit)
 
-	
 	enterTask.grid(row = 0, column = 2)
-
-				
+			
 	enterTaskField.grid(row = 1, column = 2, ipadx = 50)
 						
 	Submit.grid(row = 2, column = 2)
 		
-	
-	TextArea.grid(row = 3, column = 2, padx = 10, sticky = W)
+	tArea.grid(row = 3, column = 2, padx = 10, sticky = W)
 						
 	taskNumber.grid(row = 4, column = 2, pady = 5)
 						
